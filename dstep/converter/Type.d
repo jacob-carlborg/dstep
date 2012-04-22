3f44f60c228502f9f92ef6275b39df901045112e
Jacob Carlborg <doob@me.com>
2012-04-22 14:55:11 +0200

    Better handling of translating types.

--- dstep/converter/Type.d
+++ dstep/converter/Type.d
@@ -36,87 +36,16 @@ string convertType (Type type, bool rewriteIdToObject = true)
 		{
 			case CXType_Pointer: return convertType(type.pointee) ~ "*";
 
+			case CXType_Typedef: return convertTypedef(type);
+
 			case CXType_Record:
 			case CXType_Enum:
-			case CXType_Typedef:
+			case CXType_ObjCInterface:
 				return type.spelling;
 
 			default: return convertType(type.kind, rewriteIdToObject);
 		}
 	}
-}
-
-string convertType (CXTypeKind kind, bool rewriteIdToObject = true)
-{
-	with (CXTypeKind)
-		switch (kind)
-		{
-			case CXType_Invalid: return "";
-			case CXType_Unexposed: return "";
-			case CXType_Void: return "void";
-			case CXType_Bool: return "bool";
-			case CXType_Char_U: return "";
-			case CXType_UChar: return "byte";
-			case CXType_Char16: return "wchar";
-			case CXType_Char32: return "dchar";
-			case CXType_UShort: return "ushort";
-			case CXType_UInt: return "uint";
-			case CXType_ULong: return "c_ulong";
-			case CXType_ULongLong: return "ulong";
-			case CXType_UInt128: return "";
-			case CXType_Char_S: return "char";
-			case CXType_SChar: return "";
-			case CXType_WChar: return "wchar";
-			case CXType_Short: return "short";
-			case CXType_Int: return "int";
-			case CXType_Long: return "c_long";
-			case CXType_LongLong: return "long";
-			case CXType_Int128: return "";
-			case CXType_Float: return "float";
-			case CXType_Double: return "double";
-			case CXType_LongDouble: return "real";
-			case CXType_NullPtr: return "null";
-			case CXType_Overload: return "";
-			case CXType_Dependent: return "";
-			case CXType_ObjCId: return rewriteIdToObject ? "Object" : "id";
-			case CXType_ObjCClass: return "Class";
-			case CXType_ObjCSel: return "SEL";
-			case CXType_Complex: return "";
-			case CXType_Pointer: return "";
-			case CXType_BlockPointer: return "";
-			case CXType_LValueReference: return "";
-			case CXType_RValueReference: return "";
-			case CXType_Record: return "";
-			case CXType_Enum: return "";
-			case CXType_Typedef: return "";
-			case CXType_ObjCInterface: return "";
-			case CXType_ObjCObjectPointer: return "";
-			case CXType_FunctionNoProto: return "";
-			case CXType_FunctionProto: return "";
-			case CXType_ConstantArray: return "";
-			case CXType_Vector: return "";
-			default: return "";
-		}
-}
-
-string convertFunctionPointerType (Type type)
-{
-	return "<unimplemented>";
-}
-
-string convertObjCObjectPointerType (Type type)
-{
-	if (type.kind == CXTypeKind.CXType_ObjCObjectPointer && !type.isObjCBuiltinType)
-	{
-		auto pointee = type.pointee;
-		
-		if (pointee.spelling == "Protocol")
-			return "Protocol*";
-
-		return convertType(pointee);
-	}
-	
-	return convertType(type);
 }
 
 string convertSelector (string str, bool fullName = false)
@@ -128,4 +57,93 @@ string convertSelector (string str, bool fullName = false)
 		str = str.chomp(":");
 		
 	return convertIdentifier(str);
+}
+
+private:
+
+string convertTypedef (Type type)
+in
+{
+    assert(type.kind == CXTypeKind.CXType_Typedef);
+}
+body
+{
+    auto spelling = type.spelling;
+    
+    if (spelling == "BOOL")
+        spelling = "bool";
+
+    return spelling;
+}
+
+string convertType (CXTypeKind kind, bool rewriteIdToObject = true)
+{
+	with (CXTypeKind)
+		switch (kind)
+		{
+			case CXType_Invalid: return "<unimplemented>";
+			case CXType_Unexposed: return "<unimplemented>";
+			case CXType_Void: return "void";
+			case CXType_Bool: return "bool";
+			case CXType_Char_U: return "<unimplemented>";
+			case CXType_UChar: return "byte";
+			case CXType_Char16: return "wchar";
+			case CXType_Char32: return "dchar";
+			case CXType_UShort: return "ushort";
+			case CXType_UInt: return "uint";
+			case CXType_ULong: return "c_ulong";
+			case CXType_ULongLong: return "ulong";
+			case CXType_UInt128: return "<unimplemented>";
+			case CXType_Char_S: return "char";
+			case CXType_SChar: return "<unimplemented>";
+			case CXType_WChar: return "wchar";
+			case CXType_Short: return "short";
+			case CXType_Int: return "int";
+			case CXType_Long: return "c_long";
+			case CXType_LongLong: return "long";
+			case CXType_Int128: return "<unimplemented>";
+			case CXType_Float: return "float";
+			case CXType_Double: return "double";
+			case CXType_LongDouble: return "real";
+			case CXType_NullPtr: return "null";
+			case CXType_Overload: return "<unimplemented>";
+			case CXType_Dependent: return "<unimplemented>";
+			case CXType_ObjCId: return rewriteIdToObject ? "Object" : "id";
+			case CXType_ObjCClass: return "Class";
+			case CXType_ObjCSel: return "SEL";
+			case CXType_Complex: return "<unimplemented>";
+			case CXType_Pointer: return "<unimplemented>";
+			case CXType_BlockPointer: return "<unimplemented>";
+			case CXType_LValueReference: return "<unimplemented>";
+			case CXType_RValueReference: return "<unimplemented>";
+			case CXType_Record: return "<unimplemented>";
+			case CXType_Enum: return "<unimplemented>";
+			case CXType_Typedef: return "<unimplemented>";
+			case CXType_FunctionNoProto: return "<unimplemented>";
+			case CXType_FunctionProto: return "<unimplemented>";
+			case CXType_ConstantArray: return "<unimplemented>";
+			case CXType_Vector: return "<unimplemented>";
+			default: assert(0, "Unhandled type kind");
+		}
+}
+
+string convertFunctionPointerType (Type type)
+{
+	return "<unimplemented>";
+}
+
+string convertObjCObjectPointerType (Type type)
+in
+{
+    assert(type.kind == CXTypeKind.CXType_ObjCObjectPointer && !type.isObjCBuiltinType);
+}
+body
+{
+	auto pointee = type.pointee;
+
+	if (pointee.spelling == "Protocol")
+		return "Protocol*";
+
+    else
+        return convertType(pointee);
 }
