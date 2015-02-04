03d33f177b833f7b3e0a15cec2d279f9badf346c
Jacob Carlborg <doob@me.com>
2015-02-04 20:51:18 +0100

    Update run script.

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-./build.sh
-
-if [ "$?" = 0 ] ; then
-    osx_sdk_path="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk"
-    osx_internal_include_path="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include"
-  ./bin/dstep foo.h -o foo.d -ObjC -isysroot $osx_sdk_path -I$osx_internal_include_path
-fi
+./build.sh && ./bin/dstep foo.h -o foo.d -ObjC && foo.d
