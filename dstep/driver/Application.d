jacob-carlborg/dstep

commit 3ed7e544c5aba4be83974a443ee922dde3ed825d

Stop using tango Application framework
Biggest change needed to reduce dstep dependency count.

dstep/driver/Application.d
@@ -26,280 +26,162 @@ import clang.Index;
 import clang.TranslationUnit;
 import clang.Util;
 
+import dstep.Configuration;
+import dstep.translator.Options;
 import dstep.core.Exceptions;
 import dstep.translator.Translator;
 
-class Application : DStack.Application
+class Application
 {
-    mixin Singleton;
-
     enum Version = "0.2.2";
 
     private
     {
-        Language language;
-        string[] argsToRestore;
-        bool helpFlag;
+        Configuration config;
+    }
+
+    public this (Configuration config)
+    {
+        this.config = config;
+    }
+
+    public void run ()
+    {
+        import std.exception : enforce;
+
+        enforce!DStepException(config.inputFiles.length > 0,
+            "Must supply at least one input file");
+
+        // when only one input file is supplied, -o argument is
+        // interpreted as file path, otherwise as base directory path
+        bool singleFileInput = config.inputFiles.length == 1;
+
+        // parallel generation of D modules for each of input files
+        foreach (fileName; config.inputFiles)
+        {
+            string outputFilename;
+            
+            if (singleFileInput)
+            {
+                if (config.output.length)
+                    outputFilename = config.output;
+                else
+                    outputFilename = defaultOutputFilename(fileName, false);
+            }
+            else
+            {
+                outputFilename = Path.buildPath(config.output,
+                    defaultOutputFilename(fileName, false));
+            }
+
+            string outputDir = Path.dirName(outputFilename);
+            if (!exists(outputDir))
+                mkdirRecurse(outputDir);
+
+            auto conversionTask = task!startParsingFile(config, 
+                fileName, outputFilename);
+
+            conversionTask.executeInNewThread();
+        }
+    }
+
+    static void startParsingFile (const Configuration config, string fileName,
+        string outputFilename)
+    {
+        ParseFile(config, fileName, outputFilename)
+            .startConversion();
+    }
+
+    string defaultOutputFilename (string inputFile, bool useBaseName = true)
+    {
+        if (useBaseName)
+            return Path.setExtension(Path.baseName(inputFile), "d");
+        return Path.setExtension(inputFile, "d");
+    }
+}
+
+private struct ParseFile
+{
+    private
+    {
+        string inputFile;
+        string outputFile;
+        Index index;
+        TranslationUnit translationUnit;
+        DiagnosticVisitor diagnostics;
+
+        const Configuration config;
         Compiler compiler;
     }
 
-    protected override void run ()
+    this (const Configuration config, string inputFile,
+        string outputFile)
     {
-        handleArguments;
+        this.config = config;
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
 
-        if (arguments.argument.input.hasValue)
+    void startConversion ()
+    {
+        index = Index(false, false);
+        translationUnit = TranslationUnit.parse(
+            index,
+            inputFile,
+            config.clangParams,
+            compiler.extraHeaders);
+
+        // hope that the diagnostics below handle everything
+        // if (!translationUnit.isValid)
+        //     throw new DStepException("An unknown error occurred");
+
+        diagnostics = translationUnit.diagnostics;
+
+        if (handleDiagnostics && exists(inputFile))
         {
-            auto inputFiles = arguments.argument.input.values[1 .. $];
-            string outputDir = null;
-            if (inputFiles.length > 0)
-            {
-                outputDir = arguments.output;
-                if (arguments.output.any() && !exists(outputDir))
-                    mkdirRecurse(outputDir);
-            }
+            Options options;
+            options.outputFile = outputFile;
+            options.language = config.language;
+            options.enableComments = !config.noComments;
 
-            foreach(string fileName; inputFiles)
-            {
-                auto conversionTask = task!startParsingFile(language, argsToRestore, fileName, true,
-                                                            arguments, compiler, compilerArgs, outputDir);
-                conversionTask.executeInNewThread();
-            }
-
-            startParsingFile(language, argsToRestore, arguments.argument.input.values[0], false, arguments,
-                             compiler, compilerArgs, outputDir);
+            auto translator = new Translator(translationUnit, options);
+            translator.translate;
         }
-
-        else
-            startParsingFile(language, argsToRestore, "", false, arguments, compiler, compilerArgs, null);
     }
 
-    protected override void setupArguments ()
+    ~this ()
     {
-        arguments.sloppy = true;
-        arguments.passThrough = true;
-
-        arguments.argument("input", "input files").params(1, int.max);
-
-        arguments('o', "output", "Write output to")
-            .params(1)
-            .defaults("");
-
-        arguments('x', "language", "Treat subsequent input files as having type <language>")
-            .params(1)
-            .restrict("c", "c-header", "objective-c", "objective-c-header")
-            .on(&handleLanguage);
-
-        arguments("no-comments", "Disable translation of comments.");
-
-        arguments("objective-c", "Treat source input file as Objective-C input.");
-    }
-
-    static void startParsingFile(Language lang, string[] argsToRestore, string fileName, bool createOutputFileName,
-                                 Arguments args, Compiler comp, string[] compilerArguments, string outputDir)
-    {
-        auto parseFile = ParseFile(lang, argsToRestore, fileName, createOutputFileName,
-                                   args, comp, compilerArguments, outputDir);
-        parseFile.startConversion();
+        clean();
     }
 
 private:
 
-    void handleArguments ()
+    void clean ()
     {
-        // FIXME: Cannot use type inference here, probably a bug. Results in segfault.
-        if (arguments.rawArgs.any!((string e) => e == "-ObjC"))
-            handleObjectiveC();
+        translationUnit.dispose;
+        index.dispose;
     }
 
-    void handleObjectiveC ()
+    bool anyErrors ()
     {
-        argsToRestore ~= "-ObjC";
-        language = Language.objC;
+        return diagnostics.length > 0;
     }
 
-    void handleLanguage (string language)
+    bool handleDiagnostics ()
     {
-        switch (language)
+        bool translate = true;
+
+        foreach (diag ; diagnostics)
         {
-            case "c":
-            case "c-header":
-                this.language = Language.c;
-            break;
+            auto severity = diag.severity;
 
-            // Can't handle C++ yet
-            //
-            // case "c++":
-            // case "c++-header":
-            //     this.language = Language.cpp;
-            // break;
+            with (CXDiagnosticSeverity)
+                if (translate)
+                    translate = !(severity == CXDiagnostic_Error || severity == CXDiagnostic_Fatal);
 
-            case "objective-c":
-            case "objective-c-header":
-                this.language = Language.objC;
-            break;
-
-            default:
-                throw new DStepException(`Unrecognized language "` ~ language ~ `"`);
+            writeln(stderr, diag.format);
         }
 
-        argsToRestore ~= "-x";
-        argsToRestore ~= language;
-    }
-
-    @property string[] remainingArgs ()
-    {
-        return arguments.rawArgs[1 .. $] ~ argsToRestore;
-    }
-
-    string[] extraArgs ()
-    {
-        return compiler.extraIncludePaths.map!(e => "-I" ~ e).toArray;
-    }
-
-    string[] compilerArgs ()
-    {
-        return remainingArgs ~ extraArgs;
-    }
-
-    struct ParseFile
-    {
-        private
-        {
-            string inputFile;
-            string outputFile;
-            string outputDirectory;
-            Index index;
-            TranslationUnit translationUnit;
-            DiagnosticVisitor diagnostics;
-
-            Language language;
-            string[] argsToRestore;
-            Compiler compiler;
-            string[] compilerArgs;
-            Arguments arguments;
-        }
-
-        this(
-            Language language,
-            string[] argsToRestore,
-            string fileName,
-            bool createOutputFileName,
-            Arguments arguments,
-            Compiler compiler,
-            string[] compilerArgs,
-            string outputDir)
-        {
-            this.language = language;
-            this.argsToRestore = argsToRestore.dup;
-            this.arguments = arguments;
-            this.compiler = compiler;
-            this.compilerArgs = compilerArgs.dup;
-
-            inputFile = fileName;
-
-            if (outputDir != null)
-                outputFile = Path.buildPath(outputDir, defaultOutputFilename(false));
-            else if (!createOutputFileName && arguments.output != "")
-                outputFile = arguments.output;
-            else
-                outputFile = defaultOutputFilename();
-
-            outputDir = Path.dirName(outputFile);
-            if (!exists(outputDir))
-                mkdirRecurse(outputDir);
-        }
-
-        void startConversion ()
-        {
-            if (arguments["objective-c"])
-                argsToRestore ~= "-ObjC";
-            index = Index(false, false);
-            translationUnit = TranslationUnit.parse(
-                index,
-                inputFile,
-                compilerArgs,
-                compiler.extraHeaders);
-
-            // hope that the diagnostics below handle everything
-            // if (!translationUnit.isValid)
-            //     throw new DStepException("An unknown error occurred");
-
-            diagnostics = translationUnit.diagnostics;
-
-            if (handleDiagnostics && exists(inputFile))
-            {
-                Options options;
-                options.outputFile = outputFile;
-                options.language = language;
-                options.enableComments = !arguments["no-comments"];
-
-                auto translator = new Translator(translationUnit, options);
-
-                translator.translate;
-            }
-        }
-
-        ~this()
-        {
-            clean();
-        }
-
-    private:
-
-        string defaultOutputFilename (bool useBaseName = true)
-        {
-            if (useBaseName)
-                return Path.setExtension(Path.baseName(inputFile), "d");
-            return Path.setExtension(inputFile, "d");
-        }
-
-        void clean ()
-        {
-            translationUnit.dispose;
-            index.dispose;
-        }
-
-        bool anyErrors ()
-        {
-            return diagnostics.length > 0;
-        }
-
-        bool handleDiagnostics ()
-        {
-            bool translate = true;
-
-            foreach (diag ; diagnostics)
-            {
-                auto severity = diag.severity;
-
-                with (CXDiagnosticSeverity)
-                    if (translate)
-                        translate = !(severity == CXDiagnostic_Error || severity == CXDiagnostic_Fatal);
-
-                writeln(stderr, diag.format);
-            }
-
-            return translate;
-        }
-    }
-
-    override protected void showHelp ()
-    {
-        helpFlag = true;
-
-        println("Usage: dstep [options] <input>");
-        println("Version: ", Version);
-        println();
-        println("Options:");
-        println("    -o, --output <file>          Write output to <file>.");
-        println("    -o, --output <directory>     Write all the files to <directory>, in case of multiple input files.");
-        println("    -ObjC, --objective-c         Treat source input file as Objective-C input.");
-        println("    -x, --language <language>    Treat subsequent input files as having type <language>.");
-        println("    -h, --help                   Show this message and exit.");
-        println("    --no-comments                Disable translation of comments.");
-        println();
-        println("All options that Clang accepts can be used as well.");
-        println();
-        println("Use the `-h' flag for help.");
+        return translate;
     }
 }

clang/TranslationUnit.d
@@ -28,7 +28,7 @@ struct TranslationUnit
     static TranslationUnit parse (
         Index index,
         string sourceFilename,
-        string[] commandLineArgs = ["-Wno-missing-declarations"],
+        const string[] commandLineArgs = ["-Wno-missing-declarations"],
         CXUnsavedFile[] unsavedFiles = null,
         uint options = CXTranslationUnit_Flags.CXTranslationUnit_DetailedPreprocessingRecord)
     {

clang/Util.d
@@ -13,7 +13,7 @@ import mambo.core._;
 
 import clang.c.Index;
 
-immutable(char*)* strToCArray (string[] arr)
+immutable(char*)* strToCArray (const string[] arr)
 {
     if (!arr)
         return null;

dstep/Configuration.d
@@ -1,0 +1,29 @@
+/**
+ * Copyright: Copyright (c) 2016 Jacob Carlborg. All rights reserved.
+ * Authors: Jacob Carlborg
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+ */
+module dstep.Configuration;
+
+import dstep.translator.Options;
+
+/**
+ *  Aggregation of global configuration options affecting the program
+ */
+struct Configuration
+{
+    /// array of file names to translate to D
+    string[] inputFiles;
+
+    /// expected programming language of input files
+    Language language;
+
+    /// array of parameters needed to be forwarded to clang driver
+    string[] clangParams;
+
+    /// output file name or folder (in case there are many input files)
+    string output;
+
+    /// strip all comments while translating
+    bool noComments;
+}

dstep/core/Exceptions.d
@@ -8,8 +8,8 @@ module dstep.core.Exceptions;
 
 class DStepException : Exception
 {
-    this (string message)
+    this (string message, string file = __FILE__, size_t line = __LINE__)
     {
-        super(message);
+        super(message, file, line);
     }
 }

dstep/driver/DStep.d
@@ -1,16 +1,0 @@
-/**
- * Copyright: Copyright (c) 2011 Jacob Carlborg. All rights reserved.
- * Authors: Jacob Carlborg
- * Version: Initial created: Oct 1, 2011
- * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
- */
-module dstep.driver.DStep;
-
-import dstep.driver.Application;
-import dstep.config.Configuration;
-
-int main (string[] args)
-{
-    Application.instance.config = new Configuration;
-    return Application.start!Application(args);
-}

dstep/main.d
@@ -1,0 +1,173 @@
+/**
+ * Copyright: Copyright (c) 2016 Jacob Carlborg. All rights reserved.
+ * Authors: Jacob Carlborg
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+ */
+module dstep.main;
+
+import std.typecons : tuple, Tuple;
+import std.getopt;
+
+import dstep.Configuration;
+import dstep.translator.Options;
+import dstep.core.Exceptions;
+
+
+/**
+ *  Processes command-line arguments
+ *
+ *  Params:
+ *      args = command-line arguments
+ *
+ *  Returns:
+ *      2-element tuple with first element being aggregate struct of app
+ *      configuration and second - getopt parse result
+ */
+auto parseCLI (string[] args)
+{
+    import std.getopt;
+
+    Configuration config;
+
+    // Parse dstep own paramaters:
+
+    void parseLanguage (string param, string value)
+    {
+        config.clangParams ~= "-x";
+        config.clangParams ~= value;
+
+        switch (value)
+        {
+            case "c":
+            case "c-header":
+                config.language = Language.c;
+                break;
+            case "objective-c":
+            case "objective-c-header":
+                config.language = Language.objC;
+                break;
+            default:
+                throw new DStepException(`Unrecognized language "` ~ value ~ `"`);
+        }
+    }
+
+    bool forceObjectiveC;
+
+    auto helpInformation = getopt(
+        args,
+        std.getopt.config.passThrough,
+        std.getopt.config.caseSensitive,
+        "output|o", "Write output to", &config.output,
+        "language|x", "Treat subsequent input files as having type <language>", &parseLanguage,
+        "objective-c", "Treat source input file as Objective-C input.", &forceObjectiveC,
+        "no-comments", "Disable translation of comments", &config.noComments
+    );
+
+    // remove dstep binary name (args[0])
+    args = args[1 .. $];
+
+    // Seperate input files from clang paramaters:
+
+    foreach (arg; args)
+    {
+        if (arg[0] == '-')
+            config.clangParams ~= arg;
+        else
+            config.inputFiles ~= arg;
+    }
+
+    // Post-processing of CLI
+
+    import std.algorithm : canFind;
+
+    if (config.clangParams.canFind("-ObjC"))
+        config.language = Language.objC;
+
+    return tuple(config, helpInformation);
+}
+
+unittest
+{
+    import std.meta : AliasSeq;
+    
+    Configuration config;
+    GetoptResult getopResult;
+
+    AliasSeq!(config, getopResult) = parseCLI(
+        [ "dstep", "-Xpreprocessor", "-lsomething", "-x", "c-header", "file.h" ]); 
+    assert(config.language == Language.c);
+    assert(config.inputFiles == [ "file.h" ]);
+    assert(config.clangParams == [ "-x", "c-header", "-Xpreprocessor", "-lsomething" ]);
+    assert(config.output == "");
+
+    AliasSeq!(config, getopResult) = parseCLI(
+        [ "dstep", "-ObjC", "file2.h", "--output=folder", "file.h" ]); 
+    assert(config.language == Language.objC);
+    assert(config.inputFiles == [ "file2.h", "file.h" ]);
+    assert(config.clangParams == [ "-ObjC" ]);
+    assert(config.output == "folder");
+}
+
+version (unittest) { }
+else:
+
+/**
+ *  Application entry point, handles CLI/config and forwards to
+ *  dstep.driver.Application to do actual work.
+ */
+int main (string[] args)
+{
+    auto parseResult = parseCLI(args);
+    Configuration config = parseResult[0];
+    GetoptResult getoptResult = parseResult[1];
+
+    if (getoptResult.helpWanted)
+    {
+        showHelp(config, getoptResult);
+        return 0;
+    }
+
+    import dstep.driver.Application;
+    import std.stdio;
+
+    auto application = new Application(config);
+
+    try
+    {
+        application.run();
+    }
+    catch (DStepException e)
+    {
+        writeln("An error occurred: ", e);
+        return -1;
+    }
+    catch (Throwable e)
+    {
+        writeln("An unknown error occurred: ", e);
+        throw e;
+    }
+
+    return 0;
+}
+
+void showHelp (Configuration config, GetoptResult)
+{
+    import std.stdio;
+
+    enum Version = "v0.2.2";
+
+    writeln("Usage: dstep [options] <input>");
+    writeln("Version: ", Version);
+    writeln();
+    writeln("Options:");
+    writeln("    -o, --output <file>          Write output to <file>.");
+    writeln("    -o, --output <directory>     Write all the files to <directory>, in case of multiple input files.");
+    writeln("    -ObjC, --objective-c         Treat source input file as Objective-C input.");
+    writeln("    -x, --language <language>    Treat subsequent input files as having type <language>.");
+    writeln("    -h, --help                   Show this message and exit.");
+    writeln("    --no-comments                Disable translation of comments.");
+    writeln();
+    writeln("All options that Clang accepts can be used as well.");
+    writeln();
+    writeln("Use the `-h' flag for help.");
+}

commit 296aa24be78d21405cbc172b066db3de2ce2c375

Provide extra include flags to translation
Fix #117

Regression introduced by 3ed7e54

dstep/driver/Application.d
@@ -152,11 +152,17 @@ private struct ParseFile
 
     void startConversion ()
     {
+        import std.algorithm.iteration : map;
+        import std.array;
+
         index = Index(false, false);
+
+        auto include_flags = compiler
+            .extraIncludePaths.map!(e => "-I" ~ e).array();
         translationUnit = TranslationUnit.parse(
             index,
             inputFile,
-            config.clangParams,
+            config.clangParams ~ include_flags,
             compiler.extraHeaders);
 
         diagnostics = translationUnit.diagnostics;