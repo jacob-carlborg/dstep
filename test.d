9ecfcdecd9cbba38bdf3dda2ac207d85351470d0
Jacob Carlborg <doob@me.com>
2018-03-11 16:48:21 +0100

    Upload releases to GitHub releases

--- test.d
+++ test.d
@@ -33,49 +33,10 @@ struct TestRunner
     {
         import std.traits : EnumMembers;
 
-        activate();
-
         foreach (test ; EnumMembers!TestGroup)
             runTest!(test);
 
         stdout.flush();
-    }
-
-    string workingDirectory ()
-    {
-        if (wd.length)
-            return wd;
-
-        return wd = getcwd();
-    }
-
-    string clangBasePath ()
-    {
-        return buildNormalizedPath(workingDirectory, "clangs");
-    }
-
-    void activate ()
-    {
-        version (Windows)
-        {
-            auto src = buildNormalizedPath(workingDirectory, clang.versionedLibclang);
-            auto dest = buildNormalizedPath(workingDirectory, clang.libclang);
-
-            if (exists(dest))
-                remove(dest);
-
-            copy(src, dest);
-
-            auto staticSrc = buildNormalizedPath(workingDirectory, clang.staticVersionedLibclang);
-            auto staticDest = buildNormalizedPath(workingDirectory, clang.staticLibclang);
-
-            if (exists(staticDest))
-                remove(staticDest);
-
-            copy(staticSrc, staticDest);
-        }
-        else
-            execute(["./configure", "--llvm-path", "clangs/clang/lib"]);
     }
 
     /**
