83ca39ddccd92e4253bda59da19e82bfdcfed981
GoaLitiuM <goalitium@kapsi.fi>
2019-04-23 00:04:28 +0300

    Improve Windows build script

--- tools/generate_version.bat
+++ tools/generate_version.bat
@@ -1,7 +1,11 @@
-cd "%DUB_PACKAGE_DIR%"
+@echo off
+
+pushd "%DUB_PACKAGE_DIR%"
 
 if exist .git (
   git describe --tags > resources\VERSION
 ) else (
   echo unknown > resources\VERSION
 )
+
+popd
