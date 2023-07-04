83f066ebf7a3c54a42a9277eb1281ff8587d9d98
Jacob Carlborg <doob@me.com>
2023-07-04 15:43:43 +0200

    Update build script with latest compiler

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
     . "$HOME/.dvm/scripts/dvm" ;
-    dvm use 2.071.1
+    dvm use ldc-1.32.0
 fi
 
-dub build
+dub build --verror
 #rdmd --build-only -debug -gc -ofbin/dstep -Idstack/mambo -Idstack -L-L. -L-lclang -L-ltango -L-rpath -L. "$@" dstep/driver/DStep.d
